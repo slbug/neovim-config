417f45f9990128fef9c544f33b699c7104889e39
Alexander Grebennik <sl.bug.sl@gmail.com>
2024-07-14 07:49:43 +0200

    fix copilot keymap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,8 +35,8 @@ return {
       vim.g.copilot_workspace_folders = { '~/work' }
       vim.api.nvim_set_keymap('i', '<C-k>', '<ESC>:Copilot panel<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', '<C-k>', ':Copilot panel<CR>', { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('i', '<C-]>', '<Plug>(copilot-next)', { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('i', '<C-[>', '<Plug>(copilot-previous)', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('i', '<C-n>', '<Plug>(copilot-next)', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('i', '<C-p>', '<Plug>(copilot-previous)', { noremap = true, silent = true })
     end,
   },
   {
