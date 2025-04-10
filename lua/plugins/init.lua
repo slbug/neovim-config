26ee39a0d0349cc8e663f8873de85dfbc1a5ceb2
Alexander Grebennik <slbug@users.noreply.github.com>
2025-04-10 08:25:56 +0200

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -102,9 +102,16 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_background = 'hard'
       vim.g.gruvbox_material_enable_italic = true
       vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_foreground = 'mix'
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+      vim.g.gruvbox_material_statusline_style = 'mix'
       vim.cmd("colorscheme gruvbox-material")
     end,
   },
